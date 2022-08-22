7aabb46941f02c4f85043096c4d5493cd520e202
jojovpn <111695102+jojovpn@users.noreply.github.com>
2022-08-22 16:30:52 +0300

    Update Update.php

--- Update.php
+++ Update.php
@@ -3,4 +3,4 @@ Welcome to Firenet Demo<\/b>
 
 Whatsapp: 05xxxxxxxxx<\/u><\/b>
 
-tg Page: https://t.me/\/\/\/khalid22234<\/u><\/b><\/p>","updateList":[{"description":"Server now show realtime status after install, reboot or when inactive"},{"description":"Auto-reboot server now removed."},{"description":"OpenVPN and OpenConnect now supports Ubuntu18, Ubuntu20, Debian9, Debian10 and Debian11"}]}
+tg Page: https://t.me/\/\/wyivs<\/u><\/b><\/p>","updateList":[{"description":"Server now show realtime status after install, reboot or when inactive"},{"description":"Auto-reboot server now removed."},{"description":"OpenVPN and OpenConnect now supports Ubuntu18, Ubuntu20, Debian9, Debian10 and Debian11"}]}
